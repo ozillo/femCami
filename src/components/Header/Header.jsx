995f9384959c33ac629f58a12edadc6357e9d599
ozillo <marcmateo25@gmail.com>
2023-12-06 14:47:49 +0100

    Nav-menu Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,46 @@
 import "./Header.css"
-
 import React from 'react'
 
 const Header = () => {
   return (
-    <header>
-      <h1>Fem Cami</h1>
-
+<header>
+  <h1>Header</h1>
+    <nav className="menu-nav">
+      <ul className="menu-quisom">
+        <li><a href="#">Qui som</a>
+      <ul>
+        <li><a href="#">Equip</a></li>
+        <li><a href="#">On treballem</a></li>
+        <li><a href="#">Contacte</a></li>
+      </ul>
+      </li>
+        <li><a href="#">Serveis</a>
+        <ul>
+        <li><a href="#">Terapia ocupacional</a></li>
+        <li><a href="#">Logopedia</a></li>
+        <li><a href="#">Practiques centrades en la familia</a></li>
+        <li><a href="#">Reeducació pedagogica</a></li>
+        <li><a href="#">Reforç escolar</a></li>
+        <li><a href="#">Planificació i tecniques d'estudi</a></li>
+        <li><a href="#">Cos i moviment</a></li>
+          </ul>
+          </li>
+          <li><a href="#">Assesorament</a>
+        <ul>
+        <li><a href="#">Formació i tallers</a></li>
+        <li><a href="#">Família i escola</a></li>
+          </ul>
+          </li>
+          <li><a href="#">Col·laboradors</a>
+        <ul>
+        <li><a href="#">CRAPG</a></li>
+        <li><a href="#">Autisme amb futur</a></li>
+          </ul>
+          </li>
+          <li><a href="#">Contacte</a></li>
+        </ul>
+      
+      </nav>
     </header>
   )
 }
