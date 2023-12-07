d57fbc92c3e9a5bce6bc53afec8647a8528fd9b6
Samuelson23 <3samuelmg@gmail.com>
2023-12-07 12:35:41 +0100

    styled header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,37 +6,37 @@ const Header = () => {
   <header>
     <nav className="menu-nav">
       <ul className="menu-nav-ul">
-        <li><a href="#">Qui som</a>
+        <li className="menu-nav-main-li"><a href="#">Qui som</a>
           <ul className="submenu-ul">
-            <li><a href="#">Equip</a></li>
-            <li><a href="#">On treballem</a></li>
-            <li><a href="#">Contacte</a></li>
+            <li className="menu-li-hidden"><a href="#">Equip</a></li>
+            <li className="menu-li-hidden"><a href="#">On treballem</a></li>
+            <li className="menu-li-hidden"><a href="#">Contacte</a></li>
           </ul>
         </li>
-        <li><a href="#">Serveis</a>
+        <li className="menu-nav-main-li"><a href="#">Serveis</a>
           <ul className="submenu-ul">
-            <li><a href="#">Terapia ocupacional</a></li>
-            <li><a href="#">Logopedia</a></li>
-            <li><a href="#">Practiques centrades en la familia</a></li>
-            <li><a href="#">Reeducació pedagogica</a></li>
-            <li><a href="#">Reforç escolar</a></li>
-            <li><a href="#">Planificació i tecniques d'estudi</a></li>
-            <li><a href="#">Cos i moviment</a></li>
+            <li className="menu-li-hidden"><a href="#">Terapia ocupacional</a></li>
+            <li className="menu-li-hidden"><a href="#">Logopedia</a></li>
+            <li className="menu-li-hidden"><a href="#">Practiques centrades en la familia</a></li>
+            <li className="menu-li-hidden"><a href="#">Reeducació pedagogica</a></li>
+            <li className="menu-li-hidden"><a href="#">Reforç escolar</a></li>
+            <li className="menu-li-hidden"><a href="#">Planificació i tecniques d'estudi</a></li>
+            <li className="menu-li-hidden"><a href="#">Cos i moviment</a></li>
           </ul>
         </li>
-        <li><a href="#">Assesorament</a>
+        <li className="menu-nav-main-li"><a href="#">Assesorament</a>
           <ul className="submenu-ul">
-            <li><a href="#">Formació i tallers</a></li>
-            <li><a href="#">Família i escola</a></li>
+            <li className="menu-li-hidden"><a href="#">Formació i tallers</a></li>
+            <li className="menu-li-hidden"><a href="#">Família i escola</a></li>
           </ul>
         </li>
-        <li><a href="#">Col·laboradors</a>
+        <li className="menu-nav-main-li"><a href="#">Col·laboradors</a>
           <ul className="submenu-ul">
-            <li><a href="#">CRAPG</a></li>
-            <li><a href="#">Autisme amb futur</a></li>
+            <li className="menu-li-hidden"><a href="#">CRAPG</a></li>
+            <li className="menu-li-hidden"><a href="#">Autisme amb futur</a></li>
           </ul>
           </li>
-        <li><a href="#">Contacte</a></li>
+        <li className="menu-nav-main-li"><a href="#">Contacte</a></li>
       </ul>
       
     </nav>
