2accabe9a1e4fe8ea07ebf90d897b57d23afb33e
ozillo <marcmateo25@gmail.com>
2023-12-08 23:19:17 +0100

    Services section modified

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
     <section className="services-section">
       <figure className="figure-servici-home">
         <div className="div-figure-img-tittle">
-          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702042428/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacional_wiecsg.png" alt="img-ocupacional" />
+          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702073081/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacionalVector_mua7ha.png" />
           <div className="div-figure-info">
             <h2>Terapia ocupacional</h2>
             <p>La teràpia ocupacional es una professió sòcio-sanitària que vol promoure la major autonomia, benestar i qualitat de vida en les persones, a traves de l’ocupació.
@@ -23,13 +23,13 @@ const Services = () => {
             <h2>Pràctiques centrades en la família</h2>
             <p>Actualment estem formades per aplicar la Pràctica Centrada en la Família, un model on l’evidència científica corrobora que en l’actualitat, les intervencions amb major eficàcia i eficiència són les que tenen per objectiu millorar la qualitat de vida de les persones amb discapacitat i les seves famílies. Les pràctiques professionals han anat experimentant una transformació cap a intervencions en les quals la família participen de manera activa durant tot el pla d’intervenció, aportant el seu rol com a coneixedors i amb llarga continuïtat en la vida dels seus fills.</p>
           </div>
-          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702046772/Fem%20Cami/Servicios/Pr%C3%A0ctiques%20centrades%20en%20la%20fam%C3%ADlia/alba-cuinant_z4cdnn.png" alt="practicaServeis" />
+          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702073617/Fem%20Cami/Servicios/Terapia%20Ocupacional/practiquesFamilia_sdugpf.png" alt="practicaServeis" />
         </div>
         <button>Mes info</button>
       </figure>
       <figure className="figure-servici-home">
         <div className="div-figure-img-tittle">
-        <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702042818/Fem%20Cami/Servicios/Logopedia/logopediaServeis_xjpgrs.png" alt="logopediaServeis" />
+        <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702073081/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacionalVector_mua7ha.png" alt="logopediaServeis" />
           <div className="div-figure-info">
             <h2>Logopèdia</h2>
             <p>La Logopèdia és la disciplina que engloba l’estudi, la prevenció, la detecció, l’evaluació, diagnòtic i el tractament dels trastorns de la comunicació humana: les alteracions de la veu, de l’audició, de la parla, del llenguatge (oral, escrit, gestual); i de les funcions orofacilas i deglutòries.</p>
@@ -43,14 +43,14 @@ const Services = () => {
             <h2>Reeducació pedagògica</h2>
             <p>A part de proporcionar estratègies per millorar el funcionament escolar de l’alumne/a i en la resta d’entorns, molts cops ens trobem que els aprenentatges bàsics d’infantil no acaben de estar assolits i això dificulta poder avançar amb les habilitats i aprenetatges, com la lectura i escriptura, els conceptes i continguts matemàtics que es troben més endavant.</p>
           </div>
-          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702042818/Fem%20Cami/Servicios/Logopedia/logopediaServeis_xjpgrs.png" alt="logopediaServeis" />
+          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702073617/Fem%20Cami/Servicios/Terapia%20Ocupacional/practiquesFamilia_sdugpf.png" alt="logopediaServeis" />
 
         </div>
         <button>Mes info</button>
       </figure>
       <figure className="figure-servici-home">
         <div className="div-figure-img-tittle">
-        <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702042818/Fem%20Cami/Servicios/Logopedia/logopediaServeis_xjpgrs.png" alt="logopediaServeis" />
+        <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702073081/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacionalVector_mua7ha.png" alt="logopediaServeis" />
           <div className="div-figure-info">
             <h2>Reforç escolar</h2>
             <p>A Fem Camí duem a terme un reforç escolar personalitzat de les dificultats acadèmiques que es puguin originar durant el curs escolar de l’alumne/a, com també dels continguts que s’hagin d’anar treballant.</p>
@@ -64,14 +64,14 @@ const Services = () => {
             <h2>Planificació i tècniques d'estudi</h2>
             <p>Dins del servei específic de reforç escolar grupal per adolescents amb dificultats escolars, i dins de les intervencions educatives que duem amb adolescents amb dificultats d’aprenentatge o amb diagnòstic, duem un programa d’hàbits d’estudi, planificació i tècniques, on orientem l’alumne/a, donant pautes i estratègies d’estudi, d’organització, per tal d’afavorir en la seva autonomia de treball.</p>
           </div>
-          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702042818/Fem%20Cami/Servicios/Logopedia/logopediaServeis_xjpgrs.png" alt="logopediaServeis" />
+          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702073617/Fem%20Cami/Servicios/Terapia%20Ocupacional/practiquesFamilia_sdugpf.png" alt="logopediaServeis" />
 
         </div>
         <button>Mes info</button>
       </figure>
       <figure className="figure-servici-home">
         <div className="div-figure-img-tittle">
-        <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702042818/Fem%20Cami/Servicios/Logopedia/logopediaServeis_xjpgrs.png" alt="logopediaServeis" />
+        <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702073081/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacionalVector_mua7ha.png" alt="logopediaServeis" />
           <div className="div-figure-info">
             <h2>Reforç escolar</h2>
             <p>A Fem Camí duem a terme un reforç escolar personalitzat de les dificultats acadèmiques que es puguin originar durant el curs escolar de l’alumne/a, com també dels continguts que s’hagin d’anar treballant.</p>
@@ -86,7 +86,7 @@ const Services = () => {
             <p>A Fem Camí duem a terme un reforç escolar personalitzat de les dificultats acadèmiques que es puguin originar durant el curs escolar de l’alumne/a, com també dels continguts que s’hagin d’anar treballant.</p>
 
           </div>
-          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702042818/Fem%20Cami/Servicios/Logopedia/logopediaServeis_xjpgrs.png" alt="logopediaServeis" />
+          <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702073617/Fem%20Cami/Servicios/Terapia%20Ocupacional/practiquesFamilia_sdugpf.png" alt="logopediaServeis" />
 
         </div>
         <button>Mes info</button>
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Team = () => {
   return (
-    <section className="team-section">Team</section>
+    <section className="team-section"></section>
   )
 }
 
