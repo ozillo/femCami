03273ec0d8ddb0bf876dfbad98364a9fc46e7207
ozillo <marcmateo25@gmail.com>
2023-12-09 01:23:55 +0100

    Add array data

---
+++ src/data/data.js
@@ -0,0 +1,56 @@
+export const dataTeam = [
+    {
+        name:'Mònica',
+        subname:'Cardús Torné',
+        job:'Directora CIE FEM CAMÍ Mestra d’Educació Especial',
+        img:"https://res.cloudinary.com/dtfugozix/image/upload/v1702031375/Fem%20Cami/Team/Captura_de_pantalla_2023-12-08_a_las_11.17.59_ohl4eb.png"
+    },
+    {
+        name:'Núria',
+        subname:'Plana Soler',
+        job:'Directora CIE FEM CAMÍ Mestra d’Educació Especial',
+        img:"https://res.cloudinary.com/dtfugozix/image/upload/v1702031374/Fem%20Cami/Team/Captura_de_pantalla_2023-12-08_a_las_11.19.02_y16aad.png"
+    },
+    {
+        name:'Noelia',
+        subname:'Puerto Marquez',
+        job:'Psicopedagoga',
+        img:"https://res.cloudinary.com/dtfugozix/image/upload/v1702031374/Fem%20Cami/Team/Captura_de_pantalla_2023-12-08_a_las_11.19.14_jwjd9j.png"
+    },
+    {
+        name:'Anna',
+        subname:'Orga Corcobado',
+        job:'Pedagoga',
+        img:"https://res.cloudinary.com/dtfugozix/image/upload/v1702031375/Fem%20Cami/Team/Captura_de_pantalla_2023-12-08_a_las_11.19.35_tcsb1c.png"
+    },
+    {
+        name:'Saray',
+        subname:'Gonzalez Borras',
+        job:'Integradora social i educadora',
+        img:"https://res.cloudinary.com/dtfugozix/image/upload/v1702031374/Fem%20Cami/Team/Captura_de_pantalla_2023-12-08_a_las_11.20.01_gspqvd.png"
+    },
+    {
+        name:'Claudia',
+        subname:'Blanch Mitjans',
+        job:'Pedagoga especialista amb educació inclusiva',
+        img:"https://res.cloudinary.com/dtfugozix/image/upload/v1702031374/Fem%20Cami/Team/Captura_de_pantalla_2023-12-08_a_las_11.21.46_xrwb8z.png"
+    },
+    {
+        name:'Mireia',
+        subname:'Renom Ventura',
+        job:'Integradora Social i Educadora. Interpretació de llenguatge de signes',
+        img:"https://res.cloudinary.com/dtfugozix/image/upload/v1702031375/Fem%20Cami/Team/Captura_de_pantalla_2023-12-08_a_las_11.20.13_rki6iy.png"
+    },
+    {
+        name:'Meritxell',
+        subname:'Gallardo Rovira',
+        job:'Logopeda',
+        img:"https://res.cloudinary.com/dtfugozix/image/upload/v1702031375/Fem%20Cami/Team/Captura_de_pantalla_2023-12-08_a_las_11.21.25_r1xloh.png"
+    },
+    {
+        name:'Maria',
+        subname:'Carrión Santos',
+        job:'Terapeuta Ocupacional',
+        img:"https://res.cloudinary.com/dtfugozix/image/upload/v1702031374/Fem%20Cami/Team/Captura_de_pantalla_2023-12-08_a_las_11.22.11_tioz5s.png"
+    }
+];
