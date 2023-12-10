f0e741c95914494d33ca3e42e902a4436af4b17b
Samuelson23 <3samuelmg@gmail.com>
2023-12-10 20:01:15 +0100

    carrusel team

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -5,16 +5,47 @@ import React, { useEffect, useState } from 'react'
 const Team = () => {
   const [indexArray, setIndexArray] = useState(0)
 
+  const choosePerson = (index, btn) => {
+    btn=="menor" 
+    ? index>0
+      ? setIndexArray(index-1)
+      : setIndexArray(0)
+    : index<6
+      ? setIndexArray(index+1)
+      : setIndexArray(6)
+
+
+      if(btn=="menor" && index==0){
+        let firstPerson = document.querySelector(".figure-treballador-second")
+        let secondPerson = document.querySelector(".figure-treballador-principal")
+        firstPerson.classList.remove("figure-treballador-second")
+        firstPerson.classList.add("figure-treballador-principal")
+        secondPerson.classList.remove("figure-treballador-principal")
+        secondPerson.classList.add("figure-treballador-second")
+      }else if(btn=="mayor" && index==6){
+        let firstPerson = document.querySelector(".figure-treballador-last")
+        let secondPerson = document.querySelector(".figure-treballador-principal")
+        firstPerson.classList.remove("figure-treballador-last")
+        firstPerson.classList.add("figure-treballador-principal")
+        secondPerson.classList.remove("figure-treballador-principal")
+        secondPerson.classList.add("figure-treballador-second")
+      }else{
+        
+      }
+    
+  }
+
   useEffect(()=>{
     console.log(indexArray)
-    console.log(dataTeam[indexArray])
+    console.log(dataTeam.length)
+    //console.log(dataTeam[indexArray])
   
   },[indexArray])
   return (
     <section className="team-section">
       <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702082422/Fem%20Cami/Icon/chevron_left_FILL0_wght400_GRAD0_opsz24_eoe8kb.png" 
       alt="" 
-      onClick={()=>{indexArray==0 ? setIndexArray(indexArray) : setIndexArray(indexArray-1)}}
+      onClick={()=>choosePerson(indexArray, "menor")}
       />
       <figure className="figure-treballador-second">
         <h3>{dataTeam[indexArray].name}</h3>
@@ -24,13 +55,13 @@ const Team = () => {
         <h3>{dataTeam[indexArray+1].name}</h3>
         <img src={dataTeam[indexArray+1].img} alt={`name de {dataTeam[3].name}`} className="image-treballador" />
       </figure>
-      <figure className="figure-treballador-second">
+      <figure className="figure-treballador-last">
         <h3>{dataTeam[indexArray+2].name}</h3>
         <img src={dataTeam[indexArray+2].img} alt={`name de {dataTeam[3].name}`} className="image-treballador" />
       </figure>
       <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702082464/Fem%20Cami/Icon/chevron_right_FILL0_wght400_GRAD0_opsz24_dy4ost.png" 
       alt="" 
-      onClick={()=>{indexArray==6 ? setIndexArray(indexArray) : setIndexArray(indexArray+1)}}
+      onClick={()=>choosePerson(indexArray, "mayor")}
       />
 
     </section>
