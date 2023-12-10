a2fe92beb8277bc06ca198be9be5014050831962
ozillo <marcmateo25@gmail.com>
2023-12-10 17:50:04 +0100

    css changes size and content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,35 @@ const Footer = () => {
     <footer>
   <div className="div-footer-menu">
   <div className="div-general-footer">
+  <div className="div-menu-footer">
+    <h4>Intervenció específica en autisme</h4>
+      <ul className="footer-menu-ul">
+        <li className="footer-menu-li"><a href="#">Condició de l'espectre autista</a></li>
+        <li className="footer-menu-li"><a href="#">Intervenció psicoeducativa específica en autisme</a></li>
+        <li className="footer-menu-li"><a href="#">Tallers d'habilitats socials</a></li>  
+      </ul>
+  </div>
+  <div className="div-menu-footer">
+    <h4>Serveis</h4>
+    <ul className="footer-menu-ul">
+      <li className="footer-menu-li"><a href="#">Teràpia ocupacional</a></li>
+      <li className="footer-menu-li"><a href="#">Logopèdia</a></li> 
+      <li className="footer-menu-li"><a href="#">Pràctiques centrades en la familía</a></li>
+      <li className="footer-menu-li"><a href="#">Reeducació pedagògica</a></li>
+      <li className="footer-menu-li"><a href="#">Reforç escolar</a></li>
+      <li className="footer-menu-li"><a href="#">Planificació i tècniques d'estudi</a></li>
+      <li className="footer-menu-li"><a href="#">Cos i moviment</a></li> 
+    </ul>
+  </div>
+  <div className="div-menu-footer">
+    <h4>Assessorament i col·laboració</h4>
+    <ul className="footer-menu-ul">
+      <li className="footer-menu-li"><a href="#">Formacions i tallers</a></li>
+      <li className="footer-menu-li"><a href="#">Família i escola</a></li>       
+    </ul>
+  </div>  
+  </div>  
+  <div className="div-general2-footer">
   <div className="div-social-footer">
     <img src="https://res.cloudinary.com/dtfugozix/image/upload/v1702214925/Fem%20Cami/Icon/instagram_icon_138461_fakkj3.png" 
         alt="instagram" 
@@ -25,35 +54,7 @@ const Footer = () => {
     <h4>CIE FEM CAMÍ - Carrer Alguer, 16 - Vilafranca del Penedès - 637 059 313</h4>
   </div>
   </div>
-  <div className="div-general2-footer">
-  <div className="div-themes-footer">
-    <h4>Intervenció específica en autisme</h4>
-      <ul className="footer-menu-ul">
-        <li className="footer-menu-li"><a href="#">Condició de l'espectre autista</a></li>
-        <li className="footer-menu-li"><a href="#">Intervenció psicoeducativa específica en autisme</a></li>
-        <li className="footer-menu-li"><a href="#">Tallers d'habilitats socials</a></li>  
-      </ul>
-  </div>
-  <div className="div-section3-footer">
-    <h4>Serveis</h4>
-    <ul className="footer-menu-ul">
-      <li className="footer-menu-li"><a href="#">Teràpia ocupacional</a></li>
-      <li className="footer-menu-li"><a href="#">Logopèdia</a></li> 
-      <li className="footer-menu-li"><a href="#">Pràctiques centrades en la familía</a></li>
-      <li className="footer-menu-li"><a href="#">Reeducació pedagògica</a></li>
-      <li className="footer-menu-li"><a href="#">Reforç escolar</a></li>
-      <li className="footer-menu-li"><a href="#">Planificació i tècniques d'estudi</a></li>
-      <li className="footer-menu-li"><a href="#">Cos i moviment</a></li> 
-    </ul>
-  </div>
-  <div className="div-section4-footer">
-    <h4>Assessorament i col·laboració</h4>
-    <ul className="footer-menu-ul">
-      <li className="footer-menu-li"><a href="#">Formacions i tallers</a></li>
-      <li className="footer-menu-li"><a href="#">Família i escola</a></li>       
-    </ul>
-  </div>  
-  </div>  
+  
   </div>
     </footer>
   )
