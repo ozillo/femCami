23167f5b90358e840e29eb0e132582abf21f7833
ozillo <marcmateo25@gmail.com>
2023-12-16 18:25:32 +0100

    Add ancore links on the services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -16,7 +16,7 @@ const Services = () => {
         description={`La teràpia ocupacional es una professió sòcio-sanitària que vol promoure la major autonomia, benestar i qualitat de vida en les persones, a traves de l’ocupació.
         El/la terapeuta ocupacional fa servir l’activitat de forma terapèutica per la prevenció de les malalties i/o discapacitats, el manteniment de la salut, la rehabilitació de la funcionalitat i l’adaptació del entorn de les persones. Tot això amb l’objectiu d’aconseguir un desenvolupament i una participació satisfactoris en totes les activitats de la vida diària.`}
         image={"https://res.cloudinary.com/dtfugozix/image/upload/v1702073081/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacionalVector_mua7ha.png"}
-        links={["link1", "link2", "link3", "link4"]}
+        links={[<a href="#">Què és la terapia ocupacional?</a>, <a href="#">Objectius i metodología del servei</a>,<a href="#">Què és la integració sensorial</a>,<a href="#">Quan és necessària la teràpia ocupacional?</a>]}
         />
       </div>
 
@@ -25,7 +25,7 @@ const Services = () => {
         title={"Pràctiques centrades en la família"}
         description={`Actualment estem formades per aplicar la Pràctica Centrada en la Família, un model on l’evidència científica corrobora que en l’actualitat, les intervencions amb major eficàcia i eficiència són les que tenen per objectiu millorar la qualitat de vida de les persones amb discapacitat i les seves famílies. Les pràctiques professionals han anat experimentant una transformació cap a intervencions en les quals la família participen de manera activa durant tot el pla d’intervenció, aportant el seu rol com a coneixedors i amb llarga continuïtat en la vida dels seus fills.`}
         image={"https://res.cloudinary.com/dtfugozix/image/upload/v1702073081/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacionalVector_mua7ha.png"}
-        links={["link1", "link2", "link3", "link4"]}
+        links={[<a href="#">En que consisteixen?</a>, <a href="#">Objectius i metodología del servei</a>,<a href="#">Com les apliquem?</a>,<a href="#">Quan son necesaries?</a>]}
         />
       </div>
 
@@ -45,8 +45,8 @@ const Services = () => {
             <PrintServicio 
             title={"Logopèdia"}
             description={`La Logopèdia és la disciplina que engloba l’estudi, la prevenció, la detecció, l’evaluació, diagnòtic i el tractament dels trastorns de la comunicació humana: les alteracions de la veu, de l’audició, de la parla, del llenguatge (oral, escrit, gestual); i de les funcions orofacilas i deglutòries.`}
-            image={"https://res.cloudinary.com/dtfugozix/image/upload/v1702255755/Fem%20Cami/Vectores/logopedia_xhkedr.png"}
-            links={["link1", "link2", "link3", "link4"]}
+            image={"https://res.cloudinary.com/dtfugozix/image/upload/v1702747324/Fem%20Cami/Servicios/Logopedia/logopediavector1_f1knqk.png"}
+            links={[<a href="#">En que consisteix?</a>, <a href="#">Objectius i metodologia del servei</a>,<a href="#">Quan s'aplica?</a>]}
             />
           </div>
 
@@ -55,7 +55,7 @@ const Services = () => {
             title={"Reeducació pedagògica"}
             description={`A part de proporcionar estratègies per millorar el funcionament escolar de l’alumne/a i en la resta d’entorns, molts cops ens trobem que els aprenentatges bàsics d’infantil no acaben de estar assolits i això dificulta poder avançar amb les habilitats i aprenetatges, com la lectura i escriptura, els conceptes i continguts matemàtics que es troben més endavant.`}
             image={"https://res.cloudinary.com/dtfugozix/image/upload/v1702073081/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacionalVector_mua7ha.png"}
-            links={["link1", "link2", "link3", "link4"]}
+            links={[<a href="#">Com ho apliquem?</a>, <a href="#">Objectius i metodología del servei</a>,<a href="#">Quan ho apliquem?</a>]}
             />
           </div>
           <div className="div-figure-left">
@@ -63,7 +63,7 @@ const Services = () => {
             title={"Reforç escolar"}
             description={`A Fem Camí duem a terme un reforç escolar personalitzat de les dificultats acadèmiques que es puguin originar durant el curs escolar de l’alumne/a, com també dels continguts que s’hagin d’anar treballant.`}
             image={"https://res.cloudinary.com/dtfugozix/image/upload/v1702073081/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacionalVector_mua7ha.png"}
-            links={["link1", "link2", "link3", "link4"]}
+            links={[<a href="#">En que consisteix?</a>, <a href="#">Objectius i metodologia del servei</a>,<a href="#">Quan s'aplica?</a>]}
             />
           </div>
           <div className="div-figure-right">
@@ -71,7 +71,7 @@ const Services = () => {
             title={"Planificació i tècniques d'estudi"}
             description={`Dins del servei específic de reforç escolar grupal per adolescents amb dificultats escolars, i dins de les intervencions educatives que duem amb adolescents amb dificultats d’aprenentatge o amb diagnòstic, duem un programa d’hàbits d’estudi, planificació i tècniques, on orientem l’alumne/a, donant pautes i estratègies d’estudi, d’organització, per tal d’afavorir en la seva autonomia de treball.`}
             image={"https://res.cloudinary.com/dtfugozix/image/upload/v1702073081/Fem%20Cami/Servicios/Terapia%20Ocupacional/terapiaOcupacionalVector_mua7ha.png"}
-            links={["link1", "link2", "link3", "link4"]}
+            links={[<a href="#">Com ho apliquem?</a>,<a href="#">Quines tècniques existeixen?</a> ,<a href="#">Objectiuis i metodologia del servei</a>]}
             />
           </div>
           
