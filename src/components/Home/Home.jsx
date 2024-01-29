97b6a2470a1cf77fab7c41ea76a5d53b45199747
Samuelson23 <3samuelmg@gmail.com>
2024-01-29 11:25:28 +0100

    font poppins

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,34 +1,34 @@
-import Description from "../Description/Description"
-import DividerMain from "../DividerMain/DividerMain"
-import Services from "../Services/Services"
-import ServiciosAcordeon from "../Services/ServiciosAcordeon"
-import ServiciosAutismo from "../Services/ServiciosAutismo"
-import Team from "../Team/Team"
-import "./Home.css"
-import React from 'react'
+import Description from "../Description/Description";
+import DividerMain from "../DividerMain/DividerMain";
+import Services from "../Services/Services";
+import ServiciosAcordeon from "../Services/ServiciosAcordeon";
+import ServiciosAutismo from "../Services/ServiciosAutismo";
+import Team from "../Team/Team";
+import "./Home.css";
+import React from "react";
 
 const Home = () => {
-  return ( 
-    <>
-      <Description/>
+    return (
+        <>
+            <Description />
 
-      <div className="div-title-home title-home-serveis">
-        <p className="p-title-home-serveis">Els nostres serveis</p>
-        <div></div>
-      </div>
-      <ServiciosAcordeon/>
-      <div className="div-title-home title-home-equip">
-        <p className="p-title-home-equip">El nostre equip</p>
-        <div></div>
-      </div>
-      <Team/>
-      <div className="div-title-home title-home-autisme">
-        <p className="p-title-home-autisme">Autisme</p>
-        <div></div>
-      </div>
-      <ServiciosAutismo/>
-    </>
-  )
-}
+            <div className="div-title-home title-home-serveis">
+                <p className="p-title-home-serveis">Els nostres serveis</p>
+                <div></div>
+            </div>
+            <ServiciosAcordeon />
+            <div className="div-title-home title-home-equip">
+                <p className="p-title-home-equip">El nostre equip</p>
+                <div></div>
+            </div>
+            <Team />
+            <div className="div-title-home title-home-autisme">
+                <p className="p-title-home-autisme">Autisme</p>
+                <div></div>
+            </div>
+            <ServiciosAutismo />
+        </>
+    );
+};
 
-export default Home
+export default Home;
