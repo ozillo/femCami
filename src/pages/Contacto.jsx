8e653b44e3724a1f0c50c1b32c48d510221bea94
Samuelson23 <3samuelmg@gmail.com>
2024-02-14 09:26:58 +0100

    rework contacte page

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -11,11 +11,30 @@ const Contacto = () => {
     return (
         <>
             <section className="section-contacte-general-info">
-                <div className="div-contacte-adress">
-                    <h1>Carrer de l'Alguer </h1>
+                <img
+                    src="https://res.cloudinary.com/dtfugozix/image/upload/v1707897610/Fem%20Cami/Contacto/Pngtree_watercolor_flowers_3391392_swed1b.png"
+                    alt="image watercolor flowers"
+                    className="section-contacte-img-flower contacte-img-invert"
+                />
+                <img
+                    src="https://res.cloudinary.com/dtfugozix/image/upload/v1707897610/Fem%20Cami/Contacto/Pngtree_watercolor_flowers_3391392_swed1b.png"
+                    alt="image watercolor flowers"
+                    className="section-contacte-img-flower"
+                />
+                <h1 className="section-contacte-h1">On pots trobarnos?</h1>
+                <div className="section-contacte-div-general-info">
+                    <h3>ADRAÇA:</h3>
                     <p>Vilafranca del Penedès</p>
                     <p>08720</p>
                     <p>Barcelona</p>
+                </div>
+                <div className="section-contacte-div-general-info">
+                    <h3>TELEFON:</h3>
+                    <p>+34 789 098 399</p>
+                </div>
+                <div className="section-contacte-div-general-info">
+                    <h3>E-MAIL:</h3>
+                    <p>info@femcami.cat</p>
                 </div>
                 <div className="div-contacte-social">
                     <img
@@ -35,12 +54,30 @@ const Contacto = () => {
                     />
                 </div>
             </section>
+
             <section className="section-contacte-ubication-map">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.6395295783273!2d1.6966529!3d41.3384512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a47992a1c98405%3A0xc158007af0fd0bd8!2sCarrer%20de%20l&#39;Alguer%2C%2016%2C%2008720%20Vilafranca%20del%20Pened%C3%A8s%2C%20Barcelona!5e0!3m2!1ses!2ses!4v1707558869522!5m2!1ses!2ses"
+                    width="600"
+                    height="450"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </section>
+            <section className="section-contacte-formulario"></section>
+        </>
+    );
+};
+
+export default Contacto;
+{
+    /* <section className="section-contacte-ubication-map">
                 <div class="center">
                     <input type="checkbox" name="flipped" id="flipped" />
-                    {/* <svg class="land" viewBox="0 0 100 100">
+                    <svg class="land" viewBox="0 0 100 100">
                         <path d="M0,100 L0,70 Q35,40 25,30 Q21,21 35,17 C45,35 55,0 65,15 Q70,20 80,10 Q90,5 100,14 L100,45 L90,55 Q88,80 70,80 L67,75 Q68,55 50,75 Q45,85 47,80 Q49,95 35,100 Z" />
-                    </svg> */}
+                    </svg>
                     <label class="landmark-symbol" for="flipped">
                         <svg class="landmark" viewBox="0 0 120 200">
                             <path d="M58,200 L5,105 A 60 60 0 1 1 115 105 L62,200 Z" />
@@ -64,23 +101,8 @@ const Contacto = () => {
                         </div>
                     </label>
                 </div>
-                {/* <div className="link-mapa">
+             <div className="link-mapa">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.6009674137167!2d1.693868126581495!3d41.3392895989765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a47a2760bc3ecf%3A0xe41446028836b110!2zRmVtIENhbcOt!5e0!3m2!1ses!2ses!4v1707525921745!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div> */}
-            </section>
-            <section className="section-contacte-ubication-map">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.6395295783273!2d1.6966529!3d41.3384512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a47992a1c98405%3A0xc158007af0fd0bd8!2sCarrer%20de%20l&#39;Alguer%2C%2016%2C%2008720%20Vilafranca%20del%20Pened%C3%A8s%2C%20Barcelona!5e0!3m2!1ses!2ses!4v1707558869522!5m2!1ses!2ses"
-                    width="600"
-                    height="450"
-                    allowfullscreen=""
-                    loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"
-                ></iframe>
-            </section>
-            <section className="section-contacte-formulario"></section>
-        </>
-    );
-};
-
-export default Contacto;
+        </div> 
+            </section> */
+}
