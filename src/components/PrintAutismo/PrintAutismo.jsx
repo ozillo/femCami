069a5695fc373bb568556fcae51f9d24913bf5ab
Samuelson23 <3samuelmg@gmail.com>
2024-02-19 23:33:11 +0100

    changes

--- src/components/PrintAutismo/PrintAutismo.jsx
+++ src/components/PrintAutismo/PrintAutismo.jsx
@@ -2,11 +2,6 @@ import React, { useEffect, useRef } from "react";
 import "./PrintAutismo.css";
 import { useParams } from "react-router-dom";
 const PrintAutismo = () => {
-    const rute = useParams();
-
-    useEffect(() => {
-        console.log(rute);
-    }, []);
     return (
         <>
             <section className="section-title-main-page">
@@ -56,30 +51,29 @@ const PrintAutismo = () => {
             <section className="section-all-pages-main-page">
                 <section className="section-individual-page-info">
                     <h3 className="h3-individual-page-info">
-                        Intervencio Psicoeducativa
+                        Que es el autismo?
                         <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708363074/Fem%20Cami/Vectores/icons8-brick-80_tojhwz.png"
+                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708363072/Fem%20Cami/Vectores/icons8-tools-64_1_a3hsqb.png"
                             alt="logo titulo"
                             className="h3-individual-page-icon"
                         />
                     </h3>
                     <div className="div-individual-page-info">
                         <p className="p-idividual-page-info">
-                            La intervenció consisteix en treballar totes les
-                            àrees del desenvolupament tenint en compte les
-                            necessitats de l’infant i la família, sobretot en
-                            l’àrea comunicativa i social, les habilitats
-                            personals, d’aprenentatge i d’autonomia. Ens basem
-                            en intervencions focalitzades i programes integrals
-                            basats en evidència científica com estratègies,
-                            tècniques, mètodes i enfocs específics per persones
-                            autistes, basant-se en la psicoeducació. Duem a
-                            terme intervencions combinades per atendre la
-                            persona com ésser únic.
+                            L’autisme és una variant neurològica natural que pot
+                            comportar alteracions en el desenvolupament de
+                            diverses funcions del sistema nerviós central. Es
+                            manifesta en una diversitat d’estils cognitius i
+                            formes d’experimentar, sentir i percebre el món. Les
+                            persones en l’espectre autista poden tenir diferent
+                            habilitats, interessos i maneres de processar la
+                            informació. Es important reconèixer i respectar la
+                            neurodivergència, entenen que l’autisme és una
+                            condició de vida.
                         </p>
                         <div className="div-individual-page-divider"></div>
                         <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708369977/Fem%20Cami/Vectores/Autismo444_gkurjq.png"
+                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708369893/Fem%20Cami/Vectores/Autismo222_ypyrgu.png"
                             alt="imagen seccion que es el autismo"
                             className="img-individual-page-info"
                         />
@@ -90,64 +84,29 @@ const PrintAutismo = () => {
             <section className="section-all-pages-main-page">
                 <section className="section-individual-page-info">
                     <h3 className="h3-individual-page-info">
-                        Programa d'habilitats socials
+                        Que es el autismo?
                         <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708363074/Fem%20Cami/Vectores/icons8-bird-64_hjneu5.png"
+                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708363072/Fem%20Cami/Vectores/icons8-tools-64_1_a3hsqb.png"
                             alt="logo titulo"
                             className="h3-individual-page-icon"
                         />
                     </h3>
                     <div className="div-individual-page-info">
                         <p className="p-idividual-page-info">
-                            Les habilitats socials són un conjunt de
-                            competències conductuals que possibiliten que la
-                            persona mantingui relacions socials positives amb
-                            els altres i que afronti, de manera efectiva i
-                            adaptativa, les demandes del seu entorn social,
-                            aspectes que contribueixen significativament, per
-                            una part, a l’acceptació per part dels companys i,
-                            per altra, a un ajut i adaptació social adequat. Una
-                            de les condicions que presenten les persones amb
-                            autisme és la dificultat de comprensió i la gestió
-                            de les relacions socials. És per això que des de Fem
-                            Camí duem a terme sessions grupals per treballar les
-                            habilitats socials i personals amb professionals
-                            especialistes en autisme. A través de la interacció,
-                            comunicació i el joc treballem aspectes necessaris
-                            per el desenvolupament de la persona i la inclusió
-                            social.
+                            L’autisme és una variant neurològica natural que pot
+                            comportar alteracions en el desenvolupament de
+                            diverses funcions del sistema nerviós central. Es
+                            manifesta en una diversitat d’estils cognitius i
+                            formes d’experimentar, sentir i percebre el món. Les
+                            persones en l’espectre autista poden tenir diferent
+                            habilitats, interessos i maneres de processar la
+                            informació. Es important reconèixer i respectar la
+                            neurodivergència, entenen que l’autisme és una
+                            condició de vida.
                         </p>
                         <div className="div-individual-page-divider"></div>
                         <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708370913/Fem%20Cami/Vectores/AutismoPhoto1_wcmywp.png"
-                            alt="imagen seccion que es el autismo"
-                            className="img-individual-page-info"
-                        />
-                    </div>
-                    <button className="btn-individual-page">Coneix més</button>
-                </section>
-            </section>
-            <section className="section-all-pages-main-page">
-                <section className="section-individual-page-info">
-                    <h3 className="h3-individual-page-info">
-                        Centre de recurses CRAPG
-                        <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708363075/Fem%20Cami/Vectores/icons8-garden-100_mnaflb.png"
-                            alt="logo titulo"
-                            className="h3-individual-page-icon"
-                        />
-                    </h3>
-                    <div className="div-individual-page-info">
-                        <p className="p-idividual-page-info">
-                            Fem Camí és centre col·laborador amb l’Associació
-                            Autisme amb Futur Penedès-Garraf des de l’any 2021,
-                            compartint projectes i actualment formant part del
-                            CRAPG (Centre de Recursos d’Autisme Penedès –
-                            Garraf).
-                        </p>
-                        <div className="div-individual-page-divider"></div>
-                        <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708370586/Fem%20Cami/Vectores/Autismo888_zo5kyk.png"
+                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708369893/Fem%20Cami/Vectores/Autismo222_ypyrgu.png"
                             alt="imagen seccion que es el autismo"
                             className="img-individual-page-info"
                         />
--- src/components/PrintServeis/PrintServeis.jsx
+++ src/components/PrintServeis/PrintServeis.jsx
@@ -18,11 +18,6 @@ const PrintServeis = () => {
                 <section className="section-individual-page-info">
                     <h3 className="h3-individual-page-info-serveis">
                         Intervenció educativa
-                        <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708363075/Fem%20Cami/Vectores/icons8-garden-100_mnaflb.png"
-                            alt="logo titulo"
-                            className="h3-individual-page-icon"
-                        />
                     </h3>
                     <div className="div-individual-page-info-serveis">
                         <p className="p-idividual-page-info-serveis">
@@ -31,92 +26,6 @@ const PrintServeis = () => {
                         <div className="div-individual-page-divider-serveis"></div>
                         <img
                             src="https://res.cloudinary.com/dtfugozix/image/upload/v1708370766/Fem%20Cami/Vectores/Autismo101010_pfcqwn.png"
-                            alt="imagen seccion que es el autismo"
-                            className="img-individual-page-info"
-                        />
-                    </div>
-                    <button className="btn-individual-page-serveis">
-                        Coneix més
-                    </button>
-                </section>
-            </section>
-            <section className="section-all-pages-main-page">
-                <section className="section-individual-page-info">
-                    <h3 className="h3-individual-page-info-serveis">
-                        Logopedia
-                        <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708363076/Fem%20Cami/Vectores/icons8-tools-64_2_hua7om.png"
-                            alt="logo titulo"
-                            className="h3-individual-page-icon"
-                        />
-                    </h3>
-                    <div className="div-individual-page-info-serveis">
-                        <p className="p-idividual-page-info-serveis">
-                            La Logopèdia és la disciplina que engloba l’estudi,
-                            la prevenció, la detecció, l’avaluació, diagnòstic i
-                            el tractament dels trastorns de la comunicació
-                            humana: les alteracions de la veu, de l’audició, de
-                            la parla, del llenguatge (oral, escrit, gestual); i
-                            de les funcions orofacials i deglutòries.
-                        </p>
-                        <div className="div-individual-page-divider-serveis"></div>
-                        <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708370673/Fem%20Cami/Vectores/Autismo999_hmu0qq.png"
-                            alt="imagen seccion que es el autismo"
-                            className="img-individual-page-info"
-                        />
-                    </div>
-                    <button className="btn-individual-page-serveis">
-                        Coneix més
-                    </button>
-                </section>
-            </section>
-            <section className="section-all-pages-main-page">
-                <section className="section-individual-page-info">
-                    <h3 className="h3-individual-page-info-serveis">
-                        Terapia ocupacional
-                        <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708363075/Fem%20Cami/Vectores/icons8-strategy-64_bozesi.png"
-                            alt="logo titulo"
-                            className="h3-individual-page-icon"
-                        />
-                    </h3>
-                    <div className="div-individual-page-info-serveis">
-                        <p className="p-idividual-page-info-serveis">
-                            POR AMPLIAR
-                        </p>
-                        <div className="div-individual-page-divider-serveis"></div>
-                        <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708370142/Fem%20Cami/Vectores/Autismo66_xnvunq.png"
-                            alt="imagen seccion que es el autismo"
-                            className="img-individual-page-info"
-                        />
-                    </div>
-                    <button className="btn-individual-page-serveis">
-                        Coneix més
-                    </button>
-                </section>
-            </section>
-            <section className="section-all-pages-main-page">
-                <section className="section-individual-page-info">
-                    <h3 className="h3-individual-page-info-serveis">
-                        Reeducació pedagogica
-                        <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708363075/Fem%20Cami/Vectores/icons8-maths-teacher-teaching-algebra-quadratic-equations-to-students-96_njbu53.png"
-                            alt="logo titulo"
-                            className="h3-individual-page-icon"
-                        />
-                    </h3>
-                    <div className="div-individual-page-info-serveis">
-                        <p className="p-idividual-page-info-serveis">
-                            La reeducació és una intervenció psicopedagògica
-                            personalitzada d’aquelles dificultats que presenta
-                            l’infant en relació els aprenentatges i el rendiment
-                            acadèmic, i en el seu desenvolupament.
-                        </p>
-                        <div className="div-individual-page-divider-serveis"></div>
-                        <img
-                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1708370913/Fem%20Cami/Vectores/AutismoPhoto1_wcmywp.png"
                             alt="imagen seccion que es el autismo"
                             className="img-individual-page-info"
                         />
