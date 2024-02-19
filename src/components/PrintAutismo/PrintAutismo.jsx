e938d6e0b6a73a8bac84a03c54b64e739421d161
Samuelson23 <3samuelmg@gmail.com>
2024-02-19 11:48:12 +0100

    main page autismo

--- src/components/PrintAutismo/PrintAutismo.jsx
+++ src/components/PrintAutismo/PrintAutismo.jsx
@@ -1,9 +1,139 @@
-import React from 'react'
-
+import React, { useEffect, useRef } from "react";
+import "./PrintAutismo.css";
+import { useParams } from "react-router-dom";
 const PrintAutismo = () => {
-  return (
-    <div>PrintAutismo</div>
-  )
-}
+    const rute = useParams();
 
-export default PrintAutismo
+    useEffect(() => {
+        console.log(rute);
+    }, []);
+    return (
+        <>
+            <section className="section-title-main-page">
+                <h1 className="title-page">AUTISME</h1>
+            </section>
+            <section className="section-description-main-page">
+                <p>FEM CAMI es centro especializado en el mundo del autismo.</p>
+                <p>
+                    Somos un equipo de profesionales que tienen una trayectoria
+                    de mas de 10 años dedicados a la enseñanza.
+                </p>
+                <p>Nuestro principales valores son:</p>
+            </section>
+            <section className="section-all-pages-main-page">
+                <section className="section-individual-page-info">
+                    <h3 className="h3-individual-page-info">
+                        Que es el autismo?
+                    </h3>
+                    <div className="div-individual-page-info">
+                        <p className="p-idividual-page-info">
+                            L’autisme és una variant neurològica natural que pot
+                            comportar alteracions en el desenvolupament de
+                            diverses funcions del sistema nerviós central. Es
+                            manifesta en una diversitat d’estils cognitius i
+                            formes d’experimentar, sentir i percebre el món. Les
+                            persones en l’espectre autista poden tenir diferent
+                            habilitats, interessos i maneres de processar la
+                            informació. Es important reconèixer i respectar la
+                            neurodivergència, entenen que l’autisme és una
+                            condició de vida.
+                        </p>
+                        <div className="div-individual-page-divider"></div>
+                        <img
+                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1702118110/Fem%20Cami/portadaprova_uxuhpp.png"
+                            alt="imagen seccion que es el autismo"
+                            className="img-individual-page-info"
+                        />
+                    </div>
+                    <button className="btn-individual-page">Coneix més</button>
+                </section>
+            </section>
+            <section className="section-all-pages-main-page">
+                <section className="section-individual-page-info">
+                    <h3 className="h3-individual-page-info">
+                        Intervencio Psicoeducativa
+                    </h3>
+                    <div className="div-individual-page-info">
+                        <p className="p-idividual-page-info">
+                            L’autisme és una variant neurològica natural que pot
+                            comportar alteracions en el desenvolupament de
+                            diverses funcions del sistema nerviós central. Es
+                            manifesta en una diversitat d’estils cognitius i
+                            formes d’experimentar, sentir i percebre el món. Les
+                            persones en l’espectre autista poden tenir diferent
+                            habilitats, interessos i maneres de processar la
+                            informació. Es important reconèixer i respectar la
+                            neurodivergència, entenen que l’autisme és una
+                            condició de vida.
+                        </p>
+                        <div className="div-individual-page-divider"></div>
+                        <img
+                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1702118110/Fem%20Cami/portadaprova_uxuhpp.png"
+                            alt="imagen seccion que es el autismo"
+                            className="img-individual-page-info"
+                        />
+                    </div>
+                    <button className="btn-individual-page">Coneix més</button>
+                </section>
+            </section>
+            <section className="section-all-pages-main-page">
+                <section className="section-individual-page-info">
+                    <h3 className="h3-individual-page-info">
+                        Programa d'habilitats socials
+                    </h3>
+                    <div className="div-individual-page-info">
+                        <p className="p-idividual-page-info">
+                            L’autisme és una variant neurològica natural que pot
+                            comportar alteracions en el desenvolupament de
+                            diverses funcions del sistema nerviós central. Es
+                            manifesta en una diversitat d’estils cognitius i
+                            formes d’experimentar, sentir i percebre el món. Les
+                            persones en l’espectre autista poden tenir diferent
+                            habilitats, interessos i maneres de processar la
+                            informació. Es important reconèixer i respectar la
+                            neurodivergència, entenen que l’autisme és una
+                            condició de vida.
+                        </p>
+                        <div className="div-individual-page-divider"></div>
+                        <img
+                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1702118110/Fem%20Cami/portadaprova_uxuhpp.png"
+                            alt="imagen seccion que es el autismo"
+                            className="img-individual-page-info"
+                        />
+                    </div>
+                    <button className="btn-individual-page">Coneix més</button>
+                </section>
+            </section>
+            <section className="section-all-pages-main-page">
+                <section className="section-individual-page-info">
+                    <h3 className="h3-individual-page-info">
+                        Centre de recurses CRAPG
+                    </h3>
+                    <div className="div-individual-page-info">
+                        <p className="p-idividual-page-info">
+                            L’autisme és una variant neurològica natural que pot
+                            comportar alteracions en el desenvolupament de
+                            diverses funcions del sistema nerviós central. Es
+                            manifesta en una diversitat d’estils cognitius i
+                            formes d’experimentar, sentir i percebre el món. Les
+                            persones en l’espectre autista poden tenir diferent
+                            habilitats, interessos i maneres de processar la
+                            informació. Es important reconèixer i respectar la
+                            neurodivergència, entenen que l’autisme és una
+                            condició de vida.
+                        </p>
+                        <div className="div-individual-page-divider"></div>
+                        <img
+                            src="https://res.cloudinary.com/dtfugozix/image/upload/v1702118110/Fem%20Cami/portadaprova_uxuhpp.png"
+                            alt="imagen seccion que es el autismo"
+                            className="img-individual-page-info"
+                        />
+                    </div>
+                    <button className="btn-individual-page">Coneix més</button>
+                </section>
+            </section>
+        </>
+    );
+};
+
+export default PrintAutismo;
