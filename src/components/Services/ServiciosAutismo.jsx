bf925a45c3aa26e6bb3e72bac26ab7d008d0005e
Samuelson23 <3samuelmg@gmail.com>
2024-03-11 12:16:46 +0100

    responsive css

--- src/components/Services/ServiciosAutismo.jsx
+++ src/components/Services/ServiciosAutismo.jsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
-import React from 'react'
+import React from "react";
 import PrintServicioAutismo from "../PrintServicio/PrintServicioAutismo";
-import "./ServiciosAutismo.css"
+import "./ServiciosAutismo.css";
 import AcordeonSlider from "../AcordeonSlider/AcordeonSlider";
 
-
-
-
 const ServiciosAutismo = () => {
-  return (
-    <>
-    <section className="section-home-autismo">
-      <div className="div-info-autismo">
+    return (
+        <>
+            <section className="section-home-autismo">
+                {/* <div className="div-info-autismo">
       <div className="container">
       SOM CENTRE
       <div className="flip">
@@ -21,36 +18,51 @@ const ServiciosAutismo = () => {
       </div>
       EN AUTISME
       </div>
-      </div>
-      <div className="div-all-servicesAutismo">
-      <div className="div-service-autismo">
-      <PrintServicioAutismo
-         icon={"https://res.cloudinary.com/dtfugozix/image/upload/v1706486965/Fem%20Cami/iconos%20y%20vectores%20Divi%20Page/iconGlassesOrange_wcqdtr.png"}
-         title={"Intervenció Psicoeducativa"}
-         content={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi autem expedita accusantium sapiente tenetur labore aperiam dolorem numquam harum. Eligendi, tempore asperiores ducimus esse impedit fugit aut. Totam, saepe nisi!"}
-        />
-      </div>
-      <div className="div-service-autismo">
-      <PrintServicioAutismo
-        icon={"https://res.cloudinary.com/dtfugozix/image/upload/v1706489131/Fem%20Cami/iconos%20y%20vectores%20Divi%20Page/iconBooksBlue_iw8i5a.png"}
-        title={"Programa d'activitats socials"}
-        content={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi autem expedita accusantium sapiente tenetur labore aperiam dolorem numquam harum. Eligendi, tempore asperiores ducimus esse impedit fugit aut. Totam, saepe nisi!"}
-        
-        />
-      </div>
-      <div className="div-service-autismo">
-      <PrintServicioAutismo
-        icon={"https://res.cloudinary.com/dtfugozix/image/upload/v1706489429/Fem%20Cami/iconos%20y%20vectores%20Divi%20Page/iconBoxGreen_xb3ibo.png"}
-        title={"Centre de recursos d'autisme CRAPG"}
-        content={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi autem expedita accusantium sapiente tenetur labore aperiam dolorem numquam harum. Eligendi, tempore asperiores ducimus esse impedit fugit aut. Totam, saepe nisi!"}
-        />
-      </div>
-      </div>
-      
-    </section>
-    {/* <AcordeonSlider arrayData={arrayLogpedia}/> */}
-    </>
-  )
-}
+      </div> */}
+                <div className="div-info-autismo">
+                    <h2>CENTRE</h2>
+                    <h2>ESPECÍFIC</h2>
+                    <h2>EN AUTISME</h2>
+                </div>
+                <div className="div-all-servicesAutismo">
+                    <div className="div-service-autismo">
+                        <PrintServicioAutismo
+                            icon={
+                                "https://res.cloudinary.com/dtfugozix/image/upload/v1706486965/Fem%20Cami/iconos%20y%20vectores%20Divi%20Page/iconGlassesOrange_wcqdtr.png"
+                            }
+                            title={"Intervenció Psicoeducativa"}
+                            content={
+                                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi autem expedita accusantium sapiente tenetur labore aperiam dolorem numquam harum. Eligendi, tempore asperiores ducimus esse impedit fugit aut. Totam, saepe nisi!"
+                            }
+                        />
+                    </div>
+                    <div className="div-service-autismo">
+                        <PrintServicioAutismo
+                            icon={
+                                "https://res.cloudinary.com/dtfugozix/image/upload/v1706489131/Fem%20Cami/iconos%20y%20vectores%20Divi%20Page/iconBooksBlue_iw8i5a.png"
+                            }
+                            title={"Programa d'activitats socials"}
+                            content={
+                                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi autem expedita accusantium sapiente tenetur labore aperiam dolorem numquam harum. Eligendi, tempore asperiores ducimus esse impedit fugit aut. Totam, saepe nisi!"
+                            }
+                        />
+                    </div>
+                    <div className="div-service-autismo">
+                        <PrintServicioAutismo
+                            icon={
+                                "https://res.cloudinary.com/dtfugozix/image/upload/v1706489429/Fem%20Cami/iconos%20y%20vectores%20Divi%20Page/iconBoxGreen_xb3ibo.png"
+                            }
+                            title={"Centre de recursos d'autisme CRAPG"}
+                            content={
+                                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi autem expedita accusantium sapiente tenetur labore aperiam dolorem numquam harum. Eligendi, tempore asperiores ducimus esse impedit fugit aut. Totam, saepe nisi!"
+                            }
+                        />
+                    </div>
+                </div>
+            </section>
+            {/* <AcordeonSlider arrayData={arrayLogpedia}/> */}
+        </>
+    );
+};
 
-export default ServiciosAutismo
+export default ServiciosAutismo;
