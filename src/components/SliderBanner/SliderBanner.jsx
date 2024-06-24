6d576abc69d6e75d965425a433c047efdf6d18c2
ozillo <marcmateo25@gmail.com>
2024-06-24 16:13:49 +0200

    add new publis

--- src/components/SliderBanner/SliderBanner.jsx
+++ src/components/SliderBanner/SliderBanner.jsx
@@ -3,9 +3,11 @@ import PrintSlider from "../PrintSlider/PrintSlider";
 import "./SliderBanner.css";
 const SliderBanner = () => {
     const images = [
-        "https://res.cloudinary.com/dtfugozix/image/upload/v1706881063/Fem%20Cami/BANNER/BANNERTALLERS2_wu2x6i.png",
-        "https://res.cloudinary.com/dtfugozix/image/upload/v1706881063/Fem%20Cami/BANNER/BANNERTALLERS3_dnkcne.png",
-        "https://res.cloudinary.com/dtfugozix/image/upload/v1706881063/Fem%20Cami/BANNER/BANNERTALLERS1_hudfsp.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1719235302/Fem%20Cami/Publicaciones%20Eventos/grup_de_suport_adultses_autistes_ordi_vy1hlf.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1719234165/Fem%20Cami/Publicaciones%20Eventos/1_kpt2te.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1719234122/Fem%20Cami/Publicaciones%20Eventos/2_dvmlkm.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1719234093/Fem%20Cami/Publicaciones%20Eventos/3_viig7n.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1719234026/Fem%20Cami/Publicaciones%20Eventos/4_j0ewea.png",
     ];
 
     return (
