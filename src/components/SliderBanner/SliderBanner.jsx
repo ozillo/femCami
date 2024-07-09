f02f99bea60b69a6dfcbc1f9f10ba2b430cc7c02
ozillo <marcmateo25@gmail.com>
2024-07-09 16:37:02 +0200

    changes on responive calls

--- src/components/SliderBanner/SliderBanner.jsx
+++ src/components/SliderBanner/SliderBanner.jsx
@@ -9,10 +9,24 @@ const SliderBanner = () => {
         "https://res.cloudinary.com/dtfugozix/image/upload/v1719234093/Fem%20Cami/Publicaciones%20Eventos/3_viig7n.png",
         "https://res.cloudinary.com/dtfugozix/image/upload/v1719234026/Fem%20Cami/Publicaciones%20Eventos/4_j0ewea.png",
     ];
+    const imgTablet = [
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1720039993/Fem%20Cami/Publicaciones%20Eventos/TABLET/3_eum668.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1720039992/Fem%20Cami/Publicaciones%20Eventos/TABLET/4_ac5hch.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1720039993/Fem%20Cami/Publicaciones%20Eventos/TABLET/2_jrqqsy.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1720039991/Fem%20Cami/Publicaciones%20Eventos/TABLET/1_keh95c.png",
+    ];
+    const imgTlf = [
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1720534769/Fem%20Cami/Publicaciones%20Eventos/TELEFONO/3_khp0gc.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1720534769/Fem%20Cami/Publicaciones%20Eventos/TELEFONO/4_xtv5ih.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1720534769/Fem%20Cami/Publicaciones%20Eventos/TELEFONO/2_u6u4s4.png",
+        "https://res.cloudinary.com/dtfugozix/image/upload/v1720534770/Fem%20Cami/Publicaciones%20Eventos/TELEFONO/1_z0ucua.png",
+    ];
 
     return (
         <div className="div-slider-banner-home-page">
             <PrintSlider images={images} />
+            <PrintSlider images={imgTablet}/>
+            <PrintSlider images={imgTlf}/>
         </div>
     );
 };
